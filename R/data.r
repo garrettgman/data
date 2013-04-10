25b082a7007adcd71a0b615348332d906c125474
Garrett <grolemund@gmail.com>
2013-04-10 10:24:01 -0500

    Built dawr package, which contains the data sets used in dawr

---
+++ R/data.r
@@ -0,0 +1,53 @@
+#' A tidy version of the \code{\link[datasets]{Titanic}} data set
+#' @name tidytitanic
+#' @docType data
+#' @keywords data
+NULL
+
+#' Popular baby names from 1880-2008
+#'
+#' The top 1000 most popular boys names in the United States and the top 1000 
+#' most popular girls names in the United States for each year from 1880 - 2008
+#' 
+#' The soundex variable is a code that describes the sund of the name. Names 
+#' that sound similar will have the same soundex code.
+#' 
+#' @name bnames
+#' @docType data
+#' @keywords data
+NULL
+
+#' Number of boys and girls born in the US
+#'
+#' The number of boys and girls born in the US for each year from 1880 to 2009.
+#' @name births
+#' @docType data
+#' @keywords data
+NULL
+
+#' TB cases
+#'
+#' Number of tuberculosis cases reported by year and country. Cases are sorted 
+#' by age and gender. Data was collected by the World Health Organization.
+#' @name tb
+#' @docType data
+#' @keywords data
+NULL
+
+#' Weather in Cuernavaca, Mexico
+#'
+#' The daily maximum (TMAX) and minimum (TMIN) temperatures in Cuernavaca, 
+#' Mexico for 2010.
+#' @name weather
+#' @docType data
+#' @keywords data
+NULL
+
+#' Income levels of religious adherents
+#'
+#' A description of the income levels for adherents of different religions in 
+#' the United States. Data collected by the Pew Research Center.
+#' @name religion
+#' @docType data
+#' @keywords data
+NULL
